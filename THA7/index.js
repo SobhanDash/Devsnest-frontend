9689e2eee9e1c4340be25ec5e7b09070e45c3710
SobhanDash <sobhan7dash@gmail.com>
2021-08-13 23:01:11 +0530

    THA 7 dev done

---
+++ THA7/index.js
@@ -0,0 +1,53 @@
+const ips = document.getElementsByTagName("input");
+
+if (
+  ips[67] ||
+  ips[68] ||
+  ips[86] ||
+  ips[105] ||
+  ips[124] ||
+  ips[142] ||
+  ips[143] ||
+  ips[151] ||
+  ips[152] ||
+  ips[164] ||
+  ips[173] ||
+  ips[185] ||
+  ips[194] ||
+  ips[206] ||
+  ips[215] ||
+  ips[227] ||
+  ips[228] ||
+  ips[236] ||
+  ips[237] ||
+  ips[255] ||
+  ips[274] ||
+  ips[293] ||
+  ips[311] ||
+  ips[312]
+) {
+  ips[67].checked = "true";
+  ips[68].checked = "true";
+  ips[86].checked = "true";
+  ips[105].checked = "true";
+  ips[124].checked = "true";
+  ips[142].checked = "true";
+  ips[143].checked = "true";
+  ips[151].checked = "true";
+  ips[152].checked = "true";
+  ips[164].checked = "true";
+  ips[173].checked = "true";
+  ips[185].checked = "true";
+  ips[194].checked = "true";
+  ips[206].checked = "true";
+  ips[215].checked = "true";
+  ips[227].checked = "true";
+  ips[228].checked = "true";
+  ips[236].checked = "true";
+  ips[237].checked = "true";
+  ips[255].checked = "true";
+  ips[274].checked = "true";
+  ips[293].checked = "true";
+  ips[311].checked = "true";
+  ips[312].checked = "true";
+}
